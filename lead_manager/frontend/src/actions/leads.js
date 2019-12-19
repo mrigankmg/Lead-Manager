1478c035e69953fd832b0a3996b25957f212337e
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2019-12-18 19:09:38 -0500

    Implemented token passing once logged in/registered.

--- lead_manager/frontend/src/actions/leads.js
+++ lead_manager/frontend/src/actions/leads.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 import { createMessage, returnErrors } from './messages'
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types'
+import { tokenConfig } from './auth'
 
 //GET LEADS
-export const getLeads = () => dispatch => {
-  axios.get('/api/leads/')
+export const getLeads = () => (dispatch, getState) => {
+  axios.get('/api/leads/', tokenConfig(getState))
     .then(res => {
       dispatch({
         type: GET_LEADS,
@@ -14,8 +15,8 @@ export const getLeads = () => dispatch => {
 }
 
 //DELETE LEAD
-export const deleteLead = (id) => dispatch => {
-  axios.delete(`/api/leads/${id}/`)
+export const deleteLead = (id) => (dispatch, getState) => {
+  axios.delete(`/api/leads/${id}/`, tokenConfig(getState))
     .then(res => {
       dispatch(createMessage({ leadDeleted: 'Lead Deleted!'}))
       dispatch({
@@ -26,8 +27,8 @@ export const deleteLead = (id) => dispatch => {
 }
 
 //ADD LEAD
-export const addLead = (lead) => dispatch => {
-  axios.post("/api/leads/", lead)
+export const addLead = (lead) => (dispatch, getState) => {
+  axios.post("/api/leads/", lead, tokenConfig(getState))
     .then(res => {
       dispatch(createMessage({ leadAdded: 'Lead Added!'}))
       dispatch({
